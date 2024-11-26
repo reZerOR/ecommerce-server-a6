6194675c2228ff1a403e5b4b685a27706d77a6f7
reZerOR <gkraihan14k@gmail.com>
2024-11-26 23:10:08 +0600

    notfound route added

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,20 @@
-import { Status } from 'better-status-codes'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
-const app: Application = express()
-app.use(cors())
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+import { Status } from "better-status-codes";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import notFound from "./app/middlewares/notFound";
+const app: Application = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (_req: Request, res: Response) => {
+  res.status(Status.OK).json({
+    success: true,
+    message: "welcome to the server",
+  });
+});
+app.use(notFound);
 
-app.get('/', (_req: Request, res: Response)=>{
-    res.status(Status.OK).json({
-        success: true,
-        message: 'welcome to the server'
-    })
-})
-
-export default app
+export default app;
---
+++ src/app/middlewares/notFound.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Status } from "better-status-codes";
+import { NextFunction, Request, Response } from "express";
+
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  res.status(Status.NOT_FOUND).json({
+    success: false,
+    message: "API Not Found !!",
+    error: "",
+  });
+};
+
+export default notFound;
