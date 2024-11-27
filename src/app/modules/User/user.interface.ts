72436f9aefd4f6c222100b20946c82bd042ddb04
reZerOR <gkraihan14k@gmail.com>
2024-11-27 20:35:25 +0600

    user model added

--- src/app/modules/User/user.interface.ts
+++ src/app/modules/User/user.interface.ts
@@ -1,20 +1,21 @@
 /* eslint-disable no-unused-vars */
-import { Model } from "mongoose"
+import { Model } from "mongoose";
+import { USER_ROLE } from "./user.constant";
 
 export interface TUser {
-    _id?: string,
-    password: string,
-    role: 'admin'| 'user'
-    isDeleted: boolean,
-    name: string
-    email: string
-    phoneNumber: string
+  _id?: string;
+  password: string;
+  role: keyof typeof USER_ROLE;
+  isDeleted: boolean;
+  name: string;
+  email: string;
+  phoneNumber: string;
 }
 
-export interface IUserModel extends Model<TUser>{
-    isUserExistsByEmail(id: string): Promise<TUser>;
-    isPassWordMatched(
-        plainTextPassword: string,
-        hashedPassword: string
-    ): Promise<boolean>;
+export interface IUserModel extends Model<TUser> {
+  isUserExistsByEmail(id: string): Promise<TUser>;
+  isPassWordMatched(
+    plainTextPassword: string,
+    hashedPassword: string
+  ): Promise<boolean>;
 }
---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,47 @@
+import { model, Schema } from "mongoose";
+import { IUserModel, TUser } from "./user.interface";
+import { USER_ROLE } from "./user.constant";
+import bcrypt from "bcrypt";
+import config from "../../config";
+
+const userSchema = new Schema<TUser, IUserModel>(
+  {
+    name: { type: String, required: true },
+    role: {
+      type: String,
+      enum: Object.keys(USER_ROLE),
+      required: true,
+      default: USER_ROLE.USER,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: { type: String, required: true, select: 0 },
+    phoneNumber: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(
+    this.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+  next();
+});
+
+userSchema.statics.isUserExistsByEmail = async function (email: string) {
+  return await User.findOne({ email }).select("+password");
+};
+userSchema.statics.isPassWordMatched = async function (
+  plainTextPassword,
+  hashedPassword
+) {
+  return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+export const User = model<TUser, IUserModel>("User", userSchema);
